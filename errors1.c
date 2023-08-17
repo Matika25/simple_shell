e72c4ac02ca981ee373ad2efa96dcfae6deeb57d
Magokong97 <magokong97@gmail.com>
2023-08-17 11:16:36 -0700

    README.md

--- errors1.c
+++ errors1.c
@@ -4,7 +4,7 @@
  * _erratoi - converts a string to an integer,
  * @s: for converting the string
  * Return: 0 if no numbers in string, converted number otherwise
- *       -1 on error
+ *       :-1 on error
  */
 int _erratoi(char *s)
 {
